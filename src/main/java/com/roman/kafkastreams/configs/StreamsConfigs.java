7b53700f625c0766e48a3a7a26a1fe4b0e105bd0
Roman Matveev <r.matveev@socrat.marketing>
2024-07-18 18:49:47 +1000

    18.07.2024 -- Класс StreamsConfigs.java: описание свойств брокера для продюсера и streams топологии

--- src/main/java/com/roman/kafkastreams/configs/StreamsConfigs.java
+++ src/main/java/com/roman/kafkastreams/configs/StreamsConfigs.java
@@ -1,6 +1,28 @@
 package com.roman.kafkastreams.configs;
+import org.apache.kafka.streams.StreamsConfig;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.Properties;
 
 @Configuration
 public class StreamsConfigs {
+
+    @Bean
+    public Properties kafkaStreamsProps() {
+        Properties properties = new Properties();
+        properties.put(StreamsConfig.APPLICATION_ID_CONFIG, "my-streams-app");
+        properties.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        return properties;
+    }
+
+    @Bean
+    public Properties kafkaProducerProps() {
+        Properties properties = new Properties();
+        properties.put(StreamsConfig.APPLICATION_ID_CONFIG, "my-streams-app-2");
+        properties.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        properties.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        properties.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        return properties;
+    }
 }
