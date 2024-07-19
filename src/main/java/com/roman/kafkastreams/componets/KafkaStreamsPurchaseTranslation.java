2e99827cd674d0c8cd38696e026a9a8a76b5a752
Roman Matveev <r.matveev@socrat.marketing>
2024-07-19 10:30:21 +1000

    19.07.2024 -- Класс KafkaStreamsPurchaseTranslation.java: заполнение метода exec()

--- src/main/java/com/roman/kafkastreams/componets/KafkaStreamsPurchaseTranslation.java
+++ src/main/java/com/roman/kafkastreams/componets/KafkaStreamsPurchaseTranslation.java
@@ -1,14 +1,21 @@
 package com.roman.kafkastreams.componets;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.gson.Gson;
 import com.roman.kafkastreams.componets.intrfaces.IKafkaStreamsValueTranslation;
+import com.roman.kafkastreams.models.JsonDeserializer;
+import com.roman.kafkastreams.models.JsonSerializer;
 import com.roman.kafkastreams.models.Purchase;
 import jakarta.annotation.PreDestroy;
-import org.apache.kafka.clients.producer.Callback;
 import org.apache.kafka.clients.producer.Producer;
-import org.apache.kafka.clients.producer.ProducerRecord;
-import org.apache.kafka.clients.producer.RecordMetadata;
+import org.apache.kafka.common.serialization.Serde;
+import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.kstream.Consumed;
+import org.apache.kafka.streams.kstream.KStream;
+import org.apache.kafka.streams.kstream.Printed;
+import org.apache.kafka.streams.kstream.Produced;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
@@ -31,7 +38,28 @@ public class KafkaStreamsPurchaseTranslation implements IKafkaStreamsValueTransl
 
     @Override
     public void exec() {
+        JsonDeserializer<Purchase> purchaseJsonDeserializer = new JsonDeserializer<>(new ObjectMapper(), Purchase.class);
+        JsonSerializer<Purchase> purchaseJsonSerializer = new JsonSerializer<>();
+        Serde<Purchase> purchaseSerde = Serdes.serdeFrom(purchaseJsonSerializer, purchaseJsonDeserializer);
 
+        StreamsBuilder streamsBuilder = new StreamsBuilder();
+        KStream<String, Purchase> sourceStream = streamsBuilder.stream(INP_TOPIC, Consumed.with(Serdes.String(), purchaseSerde));
+
+        sourceStream.print(Printed.<String, Purchase>toSysOut().withLabel("input-data"));
+        KStream<String, Purchase> transformStream = sourceStream.mapValues(v ->
+                Purchase.builder()
+                        .id(v.getId())
+                        .name(v.getName().toUpperCase())
+                        .price(v.getPrice())
+                        .timestamp(v.getTimestamp())
+                        .build()
+        );
+
+        transformStream.to("output-topic", Produced.with(Serdes.String(), purchaseSerde));
+        transformStream.print(Printed.<String, Purchase>toSysOut().withLabel("output-data"));
+
+        this.kafkaStreams = new KafkaStreams(streamsBuilder.build(), this.kafkaStreamsProps);
+        this.kafkaStreams.start();
     }
 
     @Override
