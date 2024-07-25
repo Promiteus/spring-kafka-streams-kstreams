387938feb442c909c60bc4b01b6ed443be4e4c3d
Roman Matveev <r.matveev@socrat.marketing>
2024-07-25 21:46:42 +1000

    25.07.2024 -- Создание класса PurchaseJoiner.java

---
+++ src/main/java/com/roman/kafkastreams/mappers/PurchaseJoiner.java
@@ -0,0 +1,32 @@
+package com.roman.kafkastreams.mappers;
+
+import com.roman.kafkastreams.models.CorrelatePurchase;
+import com.roman.kafkastreams.models.Purchase;
+import org.apache.kafka.streams.kstream.ValueJoiner;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class PurchaseJoiner implements ValueJoiner<Purchase, Purchase, CorrelatePurchase> {
+    @Override
+    public CorrelatePurchase apply(Purchase purchase, Purchase purchase2) {
+        CorrelatePurchase.CorrelatePurchaseBuilder correlatePurchase = CorrelatePurchase.builder();
+
+        List<Purchase> purchaseList = new ArrayList<>();
+        Optional.ofNullable(purchase).ifPresent(p -> {
+            correlatePurchase.firstPurchaseDateTime(p.getTimestamp());
+            purchaseList.add(p);
+        });
+
+        Optional.ofNullable(purchase2).ifPresent(p -> {
+            correlatePurchase.secondPurchaseDateTime(p.getTimestamp());
+            purchaseList.add(p);
+        });
+
+        double totalPrice = purchaseList.size() > 0 ? purchaseList.stream().map(Purchase::getPrice).reduce(Double::sum).get() : 0;
+        correlatePurchase.totalPrice(totalPrice);
+
+        return correlatePurchase.build();
+    }
+}
--- src/main/java/com/roman/kafkastreams/models/CorrelatePurchase.java
+++ src/main/java/com/roman/kafkastreams/models/CorrelatePurchase.java
@@ -7,7 +7,6 @@ import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
 @Data
@@ -15,8 +14,8 @@ import java.util.List;
 @AllArgsConstructor
 @Builder
 public class CorrelatePurchase implements Serializable {
-    private Date firstPurchaseDateTime;
-    private Date secondPurchaseDateTime;
-    private long totalPrice = 0;
+    private long firstPurchaseDateTime = 0;
+    private long secondPurchaseDateTime = 0;
+    private double totalPrice = 0;
     private List<Purchase> purchases = new ArrayList<>();
 }
