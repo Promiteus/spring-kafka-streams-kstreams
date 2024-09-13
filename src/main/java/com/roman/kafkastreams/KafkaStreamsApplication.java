d18b33d7651987368ba3b514bf5eb66cf2547892
Roman Matveev <r.matveev@socrat.marketing>
2024-09-13 22:50:22 +1000

    13.09.2024 -- Запуск стрима и его топологии в отдельном потоке.

--- src/main/java/com/roman/kafkastreams/KafkaStreamsApplication.java
+++ src/main/java/com/roman/kafkastreams/KafkaStreamsApplication.java
@@ -1,7 +1,9 @@
 package com.roman.kafkastreams;
 
 import com.roman.kafkastreams.componets.intrfaces.IKafkaStreamTopology;
+import jakarta.annotation.PreDestroy;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.streams.KafkaStreams;
 import org.apache.kafka.streams.StreamsBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -11,6 +13,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 @Slf4j
 @EnableScheduling
 @SpringBootApplication
@@ -18,22 +23,39 @@ public class KafkaStreamsApplication {
     @Autowired
     private IKafkaStreamTopology kafkaStreamsValueTranslation;
     @Autowired
-    private StreamsBuilder streamsBuilder;
+    private KafkaStreams kafkaStreams;
+    private ExecutorService executorService;
 
     public static void main(String[] args) {
         SpringApplication.run(KafkaStreamsApplication.class, args);
+    }
+
+
+    private Runnable startStream() {
+        return () -> {
+            log.warn("Kafka Streams starting...");
+            this.kafkaStreams.start();
+        };
     }
 
     @Bean
     public CommandLineRunner runner() {
         return args -> {
             topicGenerator();
-            this.kafkaStreamsValueTranslation.process(streamsBuilder);
+            log.warn("Starting service single pool...");
+            this.executorService = Executors.newSingleThreadExecutor();
+            this.executorService.submit(this.startStream());
         };
     }
 
     @Scheduled(fixedDelay = 2000, initialDelay = 5000)
     public void topicGenerator() {
         this.kafkaStreamsValueTranslation.toTopic();
+    }
+
+    @PreDestroy
+    public void destroy() {
+        log.warn("Stopping service single pool...");
+        this.executorService.shutdownNow();
     }
 }
